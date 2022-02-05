02ea0d1ec397261fed1521e66c9783a29f647036
Emiri-Kakazu <emimail666@hotmail.co.jp>
2022-02-05 19:22:18 +0900

    Issue6_retry

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -37,7 +37,8 @@ class CalendarsController < ApplicationController
 
       wday_num = @todays_date.wday # wdayメソッドを用いて取得した数値
       days_of_week = wdays[(wday_num + x) % 7] #(wday_num + x)を7で割った余りに対応する配列wdaysの番地の値
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => days_of_week}
+      
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: days_of_week}
       @week_days.push(days)
     end
 
